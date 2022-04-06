83ea4447ed4d0334c69daba0166ca0c8890cc712
objectMaker <528154320@qq.com>
2022-04-06 20:36:18 +0800

    '测试hooks'

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,14 @@
 import React from "react";
-
+import useMouseMove from '../../Hooks/mouseMove'
 const Home = ()=>{
+    const mouseInfo = useMouseMove();
     return (
         <>
         <div>home</div>
+        <div>
+            <div>x:{mouseInfo.x}</div>
+            <div>y:{mouseInfo.y}</div>
+        </div>
         </>
     )
     }
