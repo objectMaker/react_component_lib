b59d5f9cd722d60389d01534cd042828731acd60
objectMaker <528154320@qq.com>
2022-04-08 12:51:54 +0800

    '删除原来的测试文件还有引入重置样式表'

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,12 @@
 import React from 'react';
-import { Routes, Route ,Link} from "react-router-dom";
-import Home from "./pages/Home/Home"
-import Pie from "./pages/Pie/Pie"
-import './App.css';
+import Button from './components/Button';
 
-import useMouseMove from './Hooks/mouseMove';
-import Button,{btnType} from './components/Button/Button';
-
-function App() {
-  const {x,y}= useMouseMove()
-
+const App:React.FC =()=>{
   return (
-    <div className="App">
-      <div className="outerX">x{x} </div>
-      <div>y{y}</div>
-      <nav>
-        <Link to="/pie">Pie</Link>
-        <Link to="/home">Home</Link>
-      </nav>
-      <h1>Welcome to React Router!</h1>
-      <Button btnType={btnType.Warning}>
-        我是btn
-      </Button>
-      <Button btnType={btnType.Warning} disabled={true}>
-        我是btn
-      </Button>
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="pie" element={<Pie />} />
-      </Routes>
-    </div>
-  );
-}
-
-export default App;
+    <>
+      <div>我是app</div>
+      <Button/>
+    </>
+  )
+} 
+export default App
---
+++ src/components/Button.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Button:React.FC = ()=>{
+    return (
+        <>
+            <button className="btn">
+                我是btn
+            </button>
+        </>
+    )
+}
+export default Button;
