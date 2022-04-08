9cc4154b78e8dcdf6e3617935aa6568bc83af0ea
objectMaker <528154320@qq.com>
2022-04-08 19:17:53 +0800

    '..'

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Button.scss';
+import classnames from 'classnames';
 //定义传入type类型
 type btnType =  'primary'| 'danger'|'success'| 'warning'| 'info';
 
@@ -13,22 +14,32 @@ interface iBtnProps {
     className?:string,
     style?:{
         [K:string]:string
-    }
+    },
+    href:string,
 }
 
 const Button:React.FC<iBtnProps> = (props)=>{
-    console.log(props,'props');
-    if(props.type ){
-        //如果有多传入btn的类型。就要加上相应的类型。
-        console.log('...');
+    // const className = 
+    const {type,size,disabled,link,className,style,href} = props;
+    if(link){
+        return (<>
+            <a href={href}>{props.children}</a>
+        </>)
+    }else{
+        if(props.type ){
+            //如果有多传入btn的类型。就要加上相应的className。
+            console.log('...');
+        }
+        return (
+            <>
+                <button className="btn">
+                    {props.children}
+                </button>
+            </>
+        )
     }
-    return (
-        <>
-            <button className="btn">
-                {props.children}
-            </button>
-        </>
-    )
+   
+    
 }
 //设置默认type，type的类型已经被前面的FC给固定了
 Button.defaultProps = { 
