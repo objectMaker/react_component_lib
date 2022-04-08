9d3302d85ebe9394360ca7672bd85594af54ef7f
objectMaker <528154320@qq.com>
2022-04-08 14:41:12 +0800

    'btn的默认类型修改'

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './Button.scss';
+//定义传入type类型
+type btnType =  'primary'| 'danger'|'success'| 'warning'| 'info';
+
 //定义传入数据接口类型
 interface iBtnProps {
     children?: React.ReactElement | string,
-    type?:string,
+    type?: btnType,
     size?:string,
     disabled?:boolean,
     link?:boolean,
@@ -15,7 +18,9 @@ interface iBtnProps {
 
 const Button:React.FC<iBtnProps> = (props)=>{
     console.log(props,'props');
-    
+    if(props.type ){
+        //如果有多传入btn的类型。就要加上相应的类型。
+    }
     return (
         <>
             <button className="btn">
@@ -23,5 +28,9 @@ const Button:React.FC<iBtnProps> = (props)=>{
             </button>
         </>
     )
+}
+//设置默认type，type的类型已经被前面的FC给固定了
+Button.defaultProps = { 
+    type : 'primary',
 }
 export default Button;
