814de020851588e5bd2775b9f507cb1d94c08324
objectMaker <528154320@qq.com>
2022-04-08 14:08:15 +0800

    '修改入口文件，消除警告'

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <BrowserRouter>
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container as HTMLElement);
+root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
-  </BrowserRouter>
-  ,
-  document.getElementById('root')
-);
+  );
+
+
+// ReactDOM.render(
+//   <BrowserRouter>
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+//   </BrowserRouter>
+//   ,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
