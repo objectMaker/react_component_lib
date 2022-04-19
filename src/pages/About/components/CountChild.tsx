4a8328d9b867954e382f9659f54da8b0461870fb
objectMaker <528154320@qq.com>
2022-04-19 23:29:24 +0800

    '测试部分功能'

---
+++ src/pages/About/components/CountChild.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+const  CountChild:React.FC = ()=>{
+    return <>
+        我是countChild
+        <div></div>
+    </>
+}
+export default CountChild;
---
+++ src/pages/About/components/CountFather.tsx
@@ -0,0 +1,8 @@
+import React from 'react'; 
+const  CountFather:React.FC = ()=>{
+    return <>
+        我是countFather
+        <div>首先来个数字</div>
+    </>
+}
+export default CountFather;
