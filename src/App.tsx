5dfccd3b6fd2162fa95c353a6ba23590edb2217e
objectMaker <528154320@qq.com>
2022-04-19 22:42:45 +0800

    '使用reactrouter6;

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
-import Button from './components/Button';
-import Menu from './components/Menu/Menu';
-import MenuItem from './components/MenuItem/MenuItem';
-import Count from './components/Count/Count';
+// import Button from './components/Button';
+// import Menu from './components/Menu/Menu';
+// import MenuItem from './components/MenuItem/MenuItem';
+// import Count from './components/Count/Count';
+import Home from './pages/Home/Home';
+import User from './pages/User/User';
+import About from './pages/About/About';
+import {
+  Route,
+  Link,
+  Routes
+} from "react-router-dom";
 const App:React.FC =()=>{
-  const handleClick = ()=>{
-    console.log('click出发了');
-  }
-  const handleSelect = (w:any)=>{
+  // const handleClick = ()=>{
+  //   console.log('click出发了');
+  // }
+  // const handleSelect = (w:any)=>{
     
-    console.log('你选择的',w);
+  //   console.log('你选择的',w);
     
-  }
+  // }
   return (
     <>
-      <div>我是app</div>
+      {/* <div>我是app</div>
       <Button size="large" disabled={false} onClick={handleClick}>我是你的btn呀</Button>
       <hr />
       <Menu currentIndex={0} onSelect={handleSelect}>
@@ -28,7 +36,19 @@ const App:React.FC =()=>{
           我是Item3
         </MenuItem>
       </Menu>
-      <Count/>
+      <Count/> */}
+      <Link to="about">about</Link>
+      <Link to="user">user</Link>
+      <Link to="">home</Link>
+      <Routes> 
+        <Route path="/about" element={<About/>}>
+        </Route>
+       <Route path="/user" element={<User/>}>
+        </Route>
+        <Route path="/" element={<Home/>}>
+        </Route>
+       </Routes>
+         
     </>
   )
 } 
---
+++ src/pages/About/About.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+const About:React.FC=()=>{
+    return <>
+        我是about
+    </>
+}
+export default About;
